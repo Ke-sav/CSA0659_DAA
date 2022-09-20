dfb832c9200b28e8f3e9a8ed7e226b7bfbed482a
Ke-sav <113418043+Ke-sav@users.noreply.github.com>
2022-09-20 13:24:26 +0530

    Add files via upload

--- sorting.cpp
+++ sorting.cpp
@@ -1,26 +1,28 @@
-    #include <stdio.h>
-    int main()
-    {
-        int i, j, a, n, number[30];
-        printf("Enter the value of N \n");
-        scanf("%d", &n);
-        printf("Enter the numbers \n");
-        for (i = 0; i < n; ++i)
-            scanf("%d", &number[i]);
-        for (i = 0; i < n; ++i) 
-        {
-            for (j = i + 1; j < n; ++j)
-            {
-                if (number[i] > number[j]) 
-                {
-                    a =  number[i];
-                    number[i] = number[j];
-                    number[j] = a;
-                }
-            }
-        }
-        printf("The numbers arranged in ascending order are given below \n");
-        for (i = 0; i < n; ++i)
-            printf("%d\n", number[i]);
-    }
-
+#include <stdio.h>    
+     
+int main()    
+{    
+    int arr[] = {5, 2, 8, 7, 1};     
+    int temp = 0;    
+    int length = sizeof(arr)/sizeof(arr[0]);      
+    printf("Elements of original array: \n");    
+    for (int i = 0; i < length; i++) {     
+        printf("%d ", arr[i]);     
+    }         
+    for (int i = 0; i < length; i++) {     
+        for (int j = i+1; j < length; j++) {     
+           if(arr[i] > arr[j]) {    
+               temp = arr[i];    
+               arr[i] = arr[j];    
+               arr[j] = temp;    
+           }     
+        }     
+    }    
+        
+    printf("\n");       
+    printf("Elements of array sorted in ascending order: \n");    
+    for (int i = 0; i < length; i++) {     
+        printf("%d ", arr[i]);    
+    }    
+    return 0;    
+}     
